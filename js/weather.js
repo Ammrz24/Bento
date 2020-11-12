805d4f75e061c06acc287ba1cd9e34a2d1676ca7
Ammarz24 <34020008+Ammrz24@users.noreply.github.com>
2020-11-12 18:16:11 +1100

    edited qlink
    edited qlink to only needed links

--- js/weather.js
+++ js/weather.js
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = -37.877811;
+    let longitude = 145.042313;
 
     getWeather(latitude, longitude);
 }
